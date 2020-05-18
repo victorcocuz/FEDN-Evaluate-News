2ff73c3573ec6f1aef67d3149311ec3b16be769c
Vitor Cocuz <victor.cocuz@yahoo.com>
2020-05-18 22:31:07 +0100

    feat: set up functionality for api key and import scss files

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,40 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
+
+// Dependencies
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const aylien = require("aylien_textapi");
 
+// Start App and include packages
 const app = express()
-
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+// set aylien API credentias
+const textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+  });
+
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8082, function () {
+    console.log('Example app listening on port 8082!')
 })
 
 app.get('/test', function (req, res) {
