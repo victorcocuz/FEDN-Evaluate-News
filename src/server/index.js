99521a3db8808d98bc55a05d5e6512ee1ff6024a
Vitor Cocuz <victor.cocuz@yahoo.com>
2020-05-18 23:08:56 +0100

    fix: add output to detect js iffe's and change server port

--- src/server/index.js
+++ src/server/index.js
@@ -28,8 +28,8 @@ const textapi = new aylien({
   });
 
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-    // res.sendFile(path.resolve('src/client/views/index.html'))
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
